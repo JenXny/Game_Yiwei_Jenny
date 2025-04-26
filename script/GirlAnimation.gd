580dfa739271d76c25441d506ce186431e79e4aa
Yuki9371 <cmjzdsyw@gmail.com>
2025-04-26 05:38:01 -0400

    Key added and can be collected
    Key added and can be collected. Some problems with girl's collision shape: collision shape not following character

--- script/GirlAnimation.gd
+++ script/GirlAnimation.gd
@@ -9,18 +9,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_A):
-		position.x -= 0.5
+		position.x -= 0.8
 		play("walk")
 		flip_h = true
 	elif Input.is_key_pressed(KEY_D):
-		position.x += 0.5
+		position.x += 0.8
 		play("walk")
 		flip_h = false
 	elif Input.is_key_pressed(KEY_W):
-		position.y -= 0.5
+		position.y -= 0.8
 		play("walk")
 	elif Input.is_key_pressed(KEY_S):
-		position.y += 0.5
+		position.y += 0.8
 		play("walk")
 	else:
 		play("idle")
