a6d131a182d7e7bfd2b78a2acf3e887ff76b664b
JenXny <jvivarvaldiviezo@fordham.edu>
2025-05-02 16:51:49 -0400

    changes to code

--- game_over_layer.gd
+++ game_over_layer.gd
@@ -1,5 +1,1 @@
 extends CanvasLayer
-
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://game_cover.tscn")
--- script/game.gd
+++ script/game.gd
@@ -12,7 +12,7 @@ func _on_body_entered(_body: Node2D) -> void:
 func reset_world():
 	PlayerValues.keys_amount = 0
 	PlayerValues.reset_world
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://game_over_layer.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
