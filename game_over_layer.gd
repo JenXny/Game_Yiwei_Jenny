240bc5a2f002b7922567af3ba81be538ad6dfa3e
JenXny <jvivarvaldiviezo@fordham.edu>
2025-05-02 17:52:44 -0400

    problem with game over layer
    Something is wrong so that game is not working properly

--- game_over_layer.gd
+++ game_over_layer.gd
@@ -2,3 +2,7 @@ extends CanvasLayer
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://game_cover.tscn")
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
--- win_layer.gd
+++ win_layer.gd
@@ -1,1 +1,10 @@
 extends CanvasLayer
+
+func _ready() -> void:
+	$return.connect("pressed", _on_return_pressed)
+
+func _on_return_pressed() -> void:
+	if ResourceLoader.exists("res://game_cover.tscn"):
+		get_tree().change_scene_to_file("res://game_cover.tscn")
+	else:
+		print("Error: Game scene not found!")
