c6230415e205bef389fc4869949b8dd6faab3ef2
Yuki9371 <cmjzdsyw@gmail.com>
2025-05-02 20:11:14 -0400

    Key value reset
    The amount of keys will be reset after the player wins

--- script/win_zone.gd
+++ script/win_zone.gd
@@ -2,3 +2,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://Win_layer.tscn") 
+	PlayerValues.emit_signal("reset_world", body)
--- win_layer.gd
+++ win_layer.gd
@@ -5,3 +5,4 @@ func _ready() -> void:
 
 func _on_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://game_cover.tscn")
+	
